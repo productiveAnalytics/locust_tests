65e4c254c4951ca62b62c56bcd065f7c8090b5a4
Productive Analytics <prod8ctive.analytics@gmail.com>
2022-10-11 17:58:09 -0500

    Ready to debug by capturing response

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, run_single_user
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
+
+    # for DEBUG purpose only, prefer to pass using --host
+    host = "http://localhost:5000"
 
     def __init__(self, env):
         super().__init__(env)
@@ -16,13 +19,21 @@ class QuickstartUser(HttpUser):
 
     @task
     def test_index(self):
-        self.client.get("/")
+        with self.client.get("/", catch_response=True) as resp:
+            pass # maybe set a breakpoint here to analyze the resp object?
 
     @task
     def test_time(self):
-        self.client.get("/time")
+        with self.client.get("/time", catch_response=True) as resp:
+            pass # maybe set a breakpoint here to analyze the resp object?
 
     # will have weightage of 2
     @task(2)
     def test_hello_time(self):
-        self.client.get("/time/prod8ctive")
+        with self.client.get("/time/prod8ctive", catch_response=True) as resp:
+            pass # maybe set a breakpoint here to analyze the resp object?
+
+
+# if launched directly, e.g. "python3 debugging.py", not "locust -f debugging.py"
+if __name__ == "__main__":
+    run_single_user(QuickstartUser)
